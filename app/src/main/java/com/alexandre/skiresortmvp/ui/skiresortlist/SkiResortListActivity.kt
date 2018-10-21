a0383e9f5649259785e4790428ca650a2d606ff7
Alexandre <alexandre.genet7@gmail.com>
2018-10-21 20:17:23 +0200

    add layout, strings and drawable, rename SkiResortListActivity

---
+++ app/src/main/java/com/alexandre/skiresortmvp/ui/skiresortlist/SkiResortListActivity.kt
@@ -0,0 +1,13 @@
+package com.alexandre.skiresortmvp.ui.skiresortlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.alexandre.skiresortmvp.R
+
+class SkiResortListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ski_resort_list)
+    }
+}
